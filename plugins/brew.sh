fc38beea4ffd3a768c2ff2dfb929333bd8688503
0ghny <6640409+0ghny@users.noreply.github.com>
2023-02-23 09:12:51 +0100

    brew: plugin has been moved to shellx-plugins-osx

--- plugins/brew.sh
+++
@@ -1,7 +0,0 @@
-# .............................................................................
-# if brew is available, include brew sbin into the path
-# osx brew hack
-# .............................................................................
-command -v brew >/dev/null || return
-# Settings
-export PATH="/usr/local/sbin:$PATH"
